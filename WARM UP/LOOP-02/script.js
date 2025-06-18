877817ebcf7afd0521d830845c07c3cc75fc0db3
kapilsarkar <k.sarkar81@gmail.com>
2025-06-18 16:51:50 +0530

    somepartsadded

--- WARM UP/LOOP-02/script.js
+++ WARM UP/LOOP-02/script.js
@@ -3,7 +3,7 @@
 let arr = [2, 4, 5, 0, 8, 30, 10, 25, 9];
 
 function searchElement(arr, target) {
-    for (let i = 0; i <= arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) {
             return i;
         }
@@ -12,3 +12,22 @@ function searchElement(arr, target) {
 }
 console.log(searchElement(arr, 5));
 console.log(searchElement(arr, 90));
+
+//Write a function that returns the number of negative numbers in an array
+
+let mixedArray = [2, 9, -7, -10, 20, 50, -200, 36, -55,-90,65];
+
+function checkNegativeNumber(arr) {
+    let count = 0;
+    for (let i = 0; i < mixedArray.length; i++) {
+        if (mixedArray[i] < 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+let negativeResult = checkNegativeNumber(mixedArray);
+console.log(negativeResult);
