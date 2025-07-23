f6b516b80034693c59f3507303b3ebc290f76e2b
kapilsarkar <k.sarkar81@gmail.com>
2025-07-23 08:16:20 +0530

    somepartsadded

--- ARRAYS/REMOVE DUPLICATES/script.js
+++ ARRAYS/REMOVE DUPLICATES/script.js
@@ -2,3 +2,6 @@
 
 //Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
+var RemoveDuplicates = function (nums){
+    
+}
