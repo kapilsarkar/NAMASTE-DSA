9b0e7591e88152914374cb5b937815c686db20ba
kapilsarkar <k.sarkar81@gmail.com>
2025-07-25 08:21:14 +0530

    somepartsadded

---
+++ ARRAYS/REMOVE ELEMENT/script.js
@@ -0,0 +1,8 @@
+// Problem Statement:
+
+// Given an integer array nums and an integer val, remove all occurrences of val in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+
+// Consider the number of elements in nums which are not equal to val be k. To get accepted, you need to:
+
+// Modify nums such that the first k elements contain elements not equal to val. The remaining elements beyond k do not matter. Return k.
