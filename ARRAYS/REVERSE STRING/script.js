1300af767bfa33cb07b54d4f7d6bcc3ad0baa81a
kapilsarkar <k.sarkar81@gmail.com>
2025-09-12 08:48:50 +0530

    somepartsadded

--- ARRAYS/REVERSE STRING/script.js
+++ ARRAYS/REVERSE STRING/script.js
@@ -1,1 +1,18 @@
 //Write a function that reverses a string. The input string is given as an array of characters s. You must do this by modifying the input array in-place with O(1)extra memory.
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    // Swap characters
+    [s[left], s[right]] = [s[right], s[left]];
+
+    left++;
+    right--;
+  }
+};
