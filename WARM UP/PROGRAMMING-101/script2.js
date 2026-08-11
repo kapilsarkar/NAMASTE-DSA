0ac80cb42b5fde80f2086e80fbbe7682dca3c2ff
kapilsarkar <k.sarkar81@gmail.com>
2026-08-11 19:01:47 +0530

    added

---
+++ WARM UP/PROGRAMMING-101/script2.js
@@ -0,0 +1,44 @@
+console.log("Hello World:Namaste Kapil Sarkar,Welcome Back to DSA");
+console.log(29);
+console.log(true);
+
+const a = 10;
+const b = 10;
+
+const sum = a + b;
+
+
+console.log("Addition of two numbers:", sum);
+
+let x = 20;
+x = 60;
+console.log(x);
+
+let firstName = "Kapil";
+let lastName = "Sarkar";
+let luckyNum = 56
+
+let fullName = firstName + " " + lastName + " " + luckyNum;
+console.log(fullName);
+
+
+let arr = [2, 9, 50, 30, 60, 27, 100];
+console.log(arr);
+console.log(arr[2]);
+
+let ArraySum = arr[2] + arr[5];
+console.log("Addition of Two Numbers Inside Array:", ArraySum);
+
+let newArr = ["Sachin,Virat", "John", "5", "Brock", true, ["John Cena", [200]]];
+console.log("Result of Mixed Array:", newArr[0][2]);
+
+let obj = {
+    a: 1,
+    myName: "Virat Kohli",
+    bool: true,
+    arr: [6, 7, 9]
+}
+
+console.log(obj);
+console.log(obj.myName);
+console.log(obj.arr[2]);
